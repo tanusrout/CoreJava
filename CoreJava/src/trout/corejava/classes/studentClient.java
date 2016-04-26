7dd3b045f2fb22d675242fa47746c06000122339
Tanu <tanu.s.rout@gmail.com>
2016-04-26 06:48:32 -0400

    updated

--- CoreJava/src/trout/corejava/classes/studentClient.java
+++ CoreJava/src/trout/corejava/classes/studentClient.java
@@ -4,19 +4,24 @@ public class studentClient {
 
 	public static void main(String[] args) {
 		Student studDishu = new Student();
+		studDishu.studName = "Diyansha";
 		studDishu.studId = 26;
 		studDishu.studGrade = "second";
-		studDishu.studName = "Diyansha";
+		
 		
 		
 		Student studSneha = new Student();
+		studSneha.studName = "Sneha";
 		studSneha.studId = 23;
 		studSneha.studGrade = "first";
-		studSneha.studName = "Sneha";
+	
 		
 		Student studSai = new Student();
-		studSai.studGrade = "third";
 		studSai.studName = "Sai";
+		studSai.studGrade = "third";
+	
+		Student studNiki = new Student();
+		System.out.println(studNiki.studName + "::" + studNiki.studId);
 		
 		Student studTanu = new Student();
 		studTanu = studDishu;
